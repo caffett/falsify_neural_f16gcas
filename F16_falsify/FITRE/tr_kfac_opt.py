fbe7f52be7d6b37e12c2a53ae61ea9740f8bc5d9
Zikang Xiong <zikangxiong@gmail.com>
2020-11-08 17:27:45 -0500

    2020-11-08 17:27:42

--- F16_falsify/FITRE/tr_kfac_opt.py
+++ F16_falsify/FITRE/tr_kfac_opt.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 
 from utils import *
 
+
 class AddBias(nn.Module):
     def __init__(self, bias):
         super(AddBias, self).__init__()
@@ -26,11 +27,12 @@ def init(module, weight_init, bias_init, gain=1):
     bias_init(module.bias.data)
     return module
 
-    
+
 # https://github.com/openai/baselines/blob/master/baselines/common/tf_util.py#L87
 def init_normc_(weight, gain=1):
     weight.normal_(0, 1)
     weight *= gain / torch.sqrt(weight.pow(2).sum(1, keepdim=True))
+
 
 # TODO: In order to make this code faster:
 # 1) Implement _extract_patches as a single cuda kernel
@@ -62,8 +64,8 @@ def compute_cov_a(a, classname, layer_info, fast_cnn):
         else:
             a = _extract_patches(a, *layer_info)
             a = a.view(-1, a.size(-1)).div_(a.size(1)).div_(a.size(2))
-            #print( 'Z Long form size: ', a[ 0, : ] )
-            #exit ()
+            # print( 'Z Long form size: ', a[ 0, : ] )
+            # exit ()
     elif classname == 'AddBias':
         is_cuda = a.is_cuda
         a = torch.ones(a.size(0), 1)
@@ -75,9 +77,9 @@ def compute_cov_a(a, classname, layer_info, fast_cnn):
 
 def compute_cov_g(g, classname, layer_info, fast_cnn):
     batch_size = g.size(0)
-    #print( g.size () )
-    #print( 'Raw Lambda: ', math.sqrt( group_product( g, g) ) )
-    
+    # print( g.size () )
+    # print( 'Raw Lambda: ', math.sqrt( group_product( g, g) ) )
+
     if classname == 'Conv2d':
         if fast_cnn:
             g = g.view(g.size(0), g.size(1), -1)
@@ -101,7 +103,6 @@ def update_running_stat(aa, m_aa, momentum):
     m_aa *= (1 - momentum)
 
 
-
 class SplitBias(nn.Module):
     def __init__(self, module):
         super(SplitBias, self).__init__()
@@ -113,6 +114,7 @@ class SplitBias(nn.Module):
         x = self.module(input)
         x = self.add_bias(x)
         return x
+
 
 SubProblems = {'tr': hessian_clip, 'cr': hessian_clip_cr}
 
@@ -132,7 +134,7 @@ class KFACOptimizer(optim.Optimizer):
                  Tf=10,
                  max_delta=100,
                  min_delta=1e-6,
-                ):
+                 ):
         defaults = dict()
 
         def split_bias(module):
@@ -159,20 +161,20 @@ class KFACOptimizer(optim.Optimizer):
         self.m_aa, self.m_gg = {}, {}
         self.Q_a, self.Q_g = {}, {}
         self.d_a, self.d_g = {}, {}
-        self.aa_inv, self.gg_inv = {},{}
+        self.aa_inv, self.gg_inv = {}, {}
         # self.m_a, self.m_g = {}, {}
         self.vs = []
         self.momentums = []
-        
+
         self.momentum = momentum
         self.check_grad = check_grad
         self.stat_decay = stat_decay
-        
+
         self.acc_stats = True
 
-        self.kl_clip = kl_clip # for TR radius or CR parameter
+        self.kl_clip = kl_clip  # for TR radius or CR parameter
         self.hessian_clip = SubProblems[scheme]
-        
+
         self.damping = damping
         self.weight_decay = weight_decay
 
@@ -180,15 +182,13 @@ class KFACOptimizer(optim.Optimizer):
 
         self.Ts = Ts
         self.Tf = Tf
-        
+
         self.max_delta = max_delta
         self.min_delta = min_delta
-        
+
         self.cur_loss = None
         self.next_loss = None
         self.q_model_change = None
-
-
 
     def _save_input(self, module, input):
         if self.model.training and torch.is_grad_enabled() and self.steps % self.Ts == 0:
@@ -206,7 +206,7 @@ class KFACOptimizer(optim.Optimizer):
             # Initialize buffers
             if self.steps == 0:
                 self.m_aa[module] = aa.clone()
-            
+
             update_running_stat(aa, self.m_aa[module], self.stat_decay)
 
     def _save_grad_output(self, module, grad_input, grad_output):
@@ -232,8 +232,8 @@ class KFACOptimizer(optim.Optimizer):
         for module in self.model.modules():
             classname = module.__class__.__name__
             if classname in self.known_modules:
-                assert not ((classname in ['Linear', 'Conv2d']) and module.bias is not None), \
-                                    "You must have a bias as a separate layer"
+                assert ((classname in ['Linear', 'Conv2d']) and module.bias is not None), \
+                    "You must have a bias as a separate layer"
 
                 self.modules.append(module)
                 module.register_forward_pre_hook(self._save_input)
@@ -247,16 +247,16 @@ class KFACOptimizer(optim.Optimizer):
                 # p.grad.add_(self.weight_decay, p)
         if closure:
             self.cur_loss = closure()
-        
+
         updates = {}
         for i, m in enumerate(self.modules):
-            assert len(list(m.parameters())) == 1, "Can handle only one parameter at the moment"
+            # assert len(list(m.parameters())) == 1, "Can handle only one parameter at the moment"
             classname = m.__class__.__name__
             p = next(m.parameters())
 
             la = self.damping + self.weight_decay
-            #print( 'Z[i] :', math.sqrt( group_product( self.m_aa[m], self.m_aa[m] ) ) )
-            #print( 'D[i] :', math.sqrt( group_product( self.m_gg[m], self.m_gg[m] ) ) )
+            # print( 'Z[i] :', math.sqrt( group_product( self.m_aa[m], self.m_aa[m] ) ) )
+            # print( 'D[i] :', math.sqrt( group_product( self.m_gg[m], self.m_gg[m] ) ) )
 
             if self.steps % self.Tf == 0:
                 # My asynchronous implementation exists, I will add it later.
@@ -268,11 +268,15 @@ class KFACOptimizer(optim.Optimizer):
                 # self.d_a[m].mul_((self.d_a[m] > 1e-6).float())
                 # self.d_g[m].mul_((self.d_g[m] > 1e-6).float())
                 if p.is_cuda:
-                    self.aa_inv[m] = torch.inverse(self.m_aa[m] + la*torch.eye(self.m_aa[m].shape[0]).type( torch.cuda.DoubleTensor ) )
-                    self.gg_inv[m] = torch.inverse(self.m_gg[m] + la*torch.eye(self.m_gg[m].shape[0]).type( torch.cuda.DoubleTensor ))
+                    self.aa_inv[m] = torch.inverse(
+                        self.m_aa[m] + la * torch.eye(self.m_aa[m].shape[0]).type(torch.cuda.DoubleTensor))
+                    self.gg_inv[m] = torch.inverse(
+                        self.m_gg[m] + la * torch.eye(self.m_gg[m].shape[0]).type(torch.cuda.DoubleTensor))
                 else:
-                    self.aa_inv[m] = torch.inverse(self.m_aa[m] + la*torch.eye(self.m_aa[m].shape[0]).type (torch.cuda.DoubleTensor))
-                    self.gg_inv[m] = torch.inverse(self.m_gg[m] + la*torch.eye(self.m_gg[m].shape[0]).type( torch.cuda.DoubleTensor ))
+                    self.aa_inv[m] = torch.inverse(
+                        self.m_aa[m] + la * torch.eye(self.m_aa[m].shape[0]).type(torch.FloatTensor))
+                    self.gg_inv[m] = torch.inverse(
+                        self.m_gg[m] + la * torch.eye(self.m_gg[m].shape[0]).type(torch.FloatTensor))
 
             if classname == 'Conv2d':
                 p_grad_mat = p.grad.data.view(p.grad.data.size(0), -1)
@@ -282,10 +286,10 @@ class KFACOptimizer(optim.Optimizer):
             # v2 = v1 / (
             #     self.d_g[m].unsqueeze(1) * self.d_a[m].unsqueeze(0) + la)
             # v = self.Q_g[m] @ v2 @ self.Q_a[m].t()
-            #v = self.gg_inv[m] @ p_grad_mat @ self.aa_inv[m]
+            # v = self.gg_inv[m] @ p_grad_mat @ self.aa_inv[m]
             v = self.gg_inv[m] @ p_grad_mat @ self.aa_inv[m]
             v = v.view(p.grad.data.size())
-            #print( math.sqrt( group_product(self.gg_inv[m], self.gg_inv[m] ) ), math.sqrt( group_product(p_grad_mat, p_grad_mat  ) ), math.sqrt( group_product(self.aa_inv[m], self.aa_inv[m]  ) ) )
+            # print( math.sqrt( group_product(self.gg_inv[m], self.gg_inv[m] ) ), math.sqrt( group_product(p_grad_mat, p_grad_mat  ) ), math.sqrt( group_product(self.aa_inv[m], self.aa_inv[m]  ) ) )
             updates[p] = v
 
         vs = []
@@ -296,22 +300,24 @@ class KFACOptimizer(optim.Optimizer):
             grads_fun.append(p.grad)
             grads_data.append(p.grad.data)
         self.vs = vs
-        #print( 'grad norm', math.sqrt( group_product( grads_data, grads_data ) ) )
-        #print( 'weights norm', math.sqrt( group_product( self.model.parameters (), self.model.parameters () ) ) )
-        #print( 'norm of vs (x_kfac): ', math.sqrt( group_product( vs, vs ) ) )
+        # print( 'grad norm', math.sqrt( group_product( grads_data, grads_data ) ) )
+        # print( 'weights norm', math.sqrt( group_product( self.model.parameters (), self.model.parameters () ) ) )
+        # print( 'norm of vs (x_kfac): ', math.sqrt( group_product( vs, vs ) ) )
 
-        assert(group_product(vs, grads_data) >=0)
-        vs, self.q_model_change = self.hessian_clip(grads_fun, grads_data, list(self.model.parameters()), self.kl_clip, vs, self.weight_decay)
+        assert (group_product(vs, grads_data) >= 0)
+        vs, self.q_model_change = self.hessian_clip(grads_fun, grads_data, list(self.model.parameters()), self.kl_clip,
+                                                    vs, self.weight_decay)
         if self.check_grad != 0:
-            gs, q_model_change_grad = self.hessian_clip(grads_fun, grads_data, list(self.model.parameters()), self.kl_clip, grads_data, self.weight_decay)
-            #print( 'V Model Change: ', self.q_model_change )
-            #print( 'G Model Change: ',q_model_change_grad )
-				
+            gs, q_model_change_grad = self.hessian_clip(grads_fun, grads_data, list(self.model.parameters()),
+                                                        self.kl_clip, grads_data, self.weight_decay)
+            # print( 'V Model Change: ', self.q_model_change )
+            # print( 'G Model Change: ',q_model_change_grad )
+
             if self.q_model_change > q_model_change_grad:
                 vs = gs
                 self.q_model_change = q_model_change_grad
-                #print("grad")
-        if len(self.momentums)>0 and self.momentum != 0:
+                # print("grad")
+        if len(self.momentums) > 0 and self.momentum != 0:
             # momentums, q_model_change_mom = self.hessian_clip(grads_fun, grads_data, list(self.model.parameters()), self.kl_clip, self.momentums, self.weight_decay)
             # if self.q_model_change > q_model_change_mom:
             #     vs = momentums
@@ -329,19 +335,18 @@ class KFACOptimizer(optim.Optimizer):
         # self.optim.step()
         if closure:
             self.next_loss = closure()
-            rho = (self.next_loss - self.cur_loss)/(self.q_model_change-1e-16)
+            rho = (self.next_loss - self.cur_loss) / (self.q_model_change - 1e-16)
             if rho > 0.75:
-                self.kl_clip = min(self.max_delta, 2.0*self.kl_clip)
+                self.kl_clip = min(self.max_delta, 2.0 * self.kl_clip)
             if rho < 0.25:
-                self.kl_clip = max(self.min_delta, self.kl_clip/2.0)
+                self.kl_clip = max(self.min_delta, self.kl_clip / 2.0)
             # a safeguard
-            if rho < 1e-4 or self.next_loss > 10*self.cur_loss:
-                #print("rejection")
+            if rho < 1e-4 or self.next_loss > 10 * self.cur_loss:
+                # print("rejection")
                 for i, p in enumerate(self.model.parameters()):
                     p.data.add_(1.0, vs[i])
-                #print(self.cur_loss, closure())
-            #print(rho, self.kl_clip, self.next_loss, self.cur_loss, self.q_model_change)
-            #print( '%4.10e  %4.10e  %4.10e  %4.10e %4.10e  %6s  %3.6e' %(self.cur_loss, self.next_loss, rho, self.q_model_change, math.sqrt( group_product( self.model.parameters (), self.model.parameters () ) ), 'kfac', self.kl_clip) )
+                # print(self.cur_loss, closure())
+            # print(rho, self.kl_clip, self.next_loss, self.cur_loss, self.q_model_change)
+            # print( '%4.10e  %4.10e  %4.10e  %4.10e %4.10e  %6s  %3.6e' %(self.cur_loss, self.next_loss, rho, self.q_model_change, math.sqrt( group_product( self.model.parameters (), self.model.parameters () ) ), 'kfac', self.kl_clip) )
 
-				
         self.steps += 1
